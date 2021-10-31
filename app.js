4a9960528a5a416678d491b68398f84ef57f5576
Anton Liubushko <aliubushko@lohika.com>
2021-10-31 17:45:02 +0200

    Created node server, added config, created logger.

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const { APP_PORT, ENV } = require('./config')
+const { logger } = require('./logger')
+
+const requestHandler = (request, response) => {
+  response.end('Hello World!')
+}
+const server = http.createServer(requestHandler)
+
+server.listen(APP_PORT, (err) => {
+  if (err) {
+    return logger('something bad happened', err)
+  }
+  logger(`Server is listening on ${APP_PORT}`, `ENV is `, `${ENV}`)
+})
---
+++ config.js
@@ -0,0 +1,2 @@
+module.exports.APP_PORT = process.env.APP_PORT || 3000
+module.exports.ENV = process.env
